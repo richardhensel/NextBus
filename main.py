c43fcdc3d9b9559e78ce5f5af025d9e2d86542c0
richardhensel <richardhensel20@gmail.com>
2016-08-05 08:07:01 +1000

    Work on AXAJ data transfer

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', output=defaultOutput)
 
-@app.route('/user/<GPS>')
-def user(GPS):
-    output = schedule.output(*GPS.split(','),n_stops=3,n_times=4)
+
+@app.route('/_get_location')
+def get_location():
+    print "message received"
+    lat = request.args.get('lat', 0, type=str)
+    lon = request.args.get('lon', 0, type=str)
+    print "Ajax Received"
+    output = schedule.output(lat,lon,n_stops=3,n_times=4)
     return render_template('user.html', output=output)
+
 
 
 if __name__ == "__main__":
